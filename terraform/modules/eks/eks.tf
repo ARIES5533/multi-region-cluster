2a7a17536a2416731b5dc9b449e0ea83e871acfd
ARIES5533 <olumokomoses94@gmail.com>
2024-09-19 14:02:07 +0100

    comment added

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -44,6 +44,10 @@ module "eks" {
 
 }
 
+
+##############################################################
+
+
 module "eks-secondary" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.13.1"
